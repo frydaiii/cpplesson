6b965e649a7e0375bfa2abd42c0c37c61558ee10
frydaiii <manhsuper171@gmail.com>
2023-04-21 14:47:41 +0700

    update code

--- task1.cc
+++ task1.cc
@@ -43,8 +43,8 @@ void hard_work()
 
 void chicken_and_egg()
 {
-	// using Zoo::Chicken;
-	// typedef Zoo::Egg Egg;
+	using Zoo::Chicken;
+	typedef Zoo::Egg Egg;
 	using namespace Zoo;
 	Chicken chicken;
 	Egg egg;
